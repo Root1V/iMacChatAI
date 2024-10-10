2c93623243d7a9ac807570de5346678200b0b0d4
Emeric Espiritu <emericespiritusantiago@gmail.com>
2024-10-09 20:59:26 -0500

    Initial Commit

---
+++ ChatiMac/ChatiMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatiMacApp.swift
+//  ChatiMac
+//
+//  Created by Emeric on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatiMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
